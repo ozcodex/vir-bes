c64f668ffbf4e5002fbd6baa15cc74350221ee38
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-23 17:16:42 -0500

    Include library
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

---
+++ Tamagotchi.ino
@@ -0,0 +1,12 @@
+#include <U8x8lib.h>
+#include <U8g2lib.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
