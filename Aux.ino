c6dcaab18fee83febeac17c5ee15c8e570bcbc0e
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-29 00:41:09 -0500

    Split code in files
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- Aux.ino
+++ Aux.ino
@@ -1,6 +1,16 @@
+//Fix the input between 0 and 2
 int delta(int number){
-  //fix the input between 0 and 2
   if (number > 2) number = 2;
   if (number < 0) number = 0;
   return number;
 }
+
+//Prepare the selected option to be rendered
+void build_selected(){
+  //read sprite from PROGMEM
+  memcpy_P(bits_buff, menu[selected], 100);
+  //invert each bit
+  for(int i=0;i<100;i++){
+    selected_bits[i] = ~bits_buff[i];
+    }
+  }
